cd6a5651f8bc001b5e7736c0dd4b8a87baeaf9b2
Dev-Suvo <subodipdey20@gmail.com>
2026-05-28 21:14:29 +0530

    views and from pt1

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
 
 
 def index(request):
+
+    if request.method == "POST":
+        description = request.POST.get('description')
+        amount = request.POST.get('amount')
+
+        description = description.strip()
+        
+        if not description:
+            messages.info(request, "Description cannot be blank!!")
+            return redirect('/')
+        
+        if not amount:
+            messages.info(request, "Amount is required!!")
+            return redirect('/')
+        
+
+        try:
+            amount = float(amount)
+            print(amount,type(amount),description,type(description))
+
+        except ValueError:
+            messages.info(request, "Please enter a valid number!!")
+            return redirect('/')
+        
+        
+
+        return redirect('/')
+
+
+
+
+
     return render(request, 'index.html')
